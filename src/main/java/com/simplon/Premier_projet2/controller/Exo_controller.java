31ec5d8ed8f24a873b1e6d8284bebcd5786d9d29
franck <franckkoukougnon@yahoo.fr>
2022-12-14 23:42:52 +0100

    spring lesson

--- src/main/java/com/simplon/Premier_projet2/controller/Exo_controller.java
+++ src/main/java/com/simplon/Premier_projet2/controller/Exo_controller.java
@@ -1,6 +1,5 @@
 package com.simplon.Premier_projet2.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Exo_controller {
     @ResponseBody
     @RequestMapping(path = "/monexo")
-    public String getExo(){
+    public String getExo() {
         System.out.println("Mon exo Get Spring");
         return " Mon Exo Get Walks";
 
